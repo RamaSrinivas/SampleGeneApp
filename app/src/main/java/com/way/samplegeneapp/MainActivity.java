fa9b738809ea07e64e78efebe4f4ae3b041b013f
RamaSrinivasM <srinivas.m@way2online.co.in>
2019-06-24 16:05:17 +0530

    version 2

--- app/src/main/java/com/way/samplegeneapp/MainActivity.java
+++ app/src/main/java/com/way/samplegeneapp/MainActivity.java
@@ -6,14 +6,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements GeneDataCallbacks{
+import com.gene.GeneApp;
+import com.gene.GeneListeners.GeneDataCallbacks;
+
+public class MainActivity extends AppCompatActivity implements GeneDataCallbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        //Replace with your keys
-        GeneApp.initialize(MainActivity.this, "YOUR_API_KEY", "YOUR_TRANSACTION_NAME", "USER_GOOGLE_ADVERTISER_ID", "USER_DETAILS", "USER_TRAITS");
+        GeneApp.initialize(MainActivity.this, "YOUR_API_KEY",  "USER_GOOGLE_ADVERTISER_ID");
+        GeneApp.start("testing transaction");
+        GeneApp.age("26", "number");
+        GeneApp.gender("male", "String");
+        GeneApp.locationCity("hyderabad", "String");
+        GeneApp.locationState("telengana", "String");
+        GeneApp.locationCountry("India", "String");
+        GeneApp.zipcode("500090", "number");
+        GeneApp.addtrait("from", "hyderabad", "string");
+        GeneApp.addtrait("to", "vijayawada", "string");
+        GeneApp.addtrait("b_date", "30-05-2019", "date");
+        GeneApp.addtrait("b_time", "123123123", "number");
+        GeneApp.send();
     }
 
     @Override
